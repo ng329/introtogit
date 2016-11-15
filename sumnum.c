4ec3b2a906d054329f7e2c995b6b775eff22a414
ng329 <ng329@exeter.ac.uk>
2016-11-15 14:03:15 +0000

    Changed variable sum to SumofTwoNums

--- sumnum.c
+++ sumnum.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 int main()
 {
-    int n1, n2, Sum;
+    int n1, n2, SumOfTwoNums;
     
     printf("Enter two integers: ");
 
     // two integers entered by user are stored using scanf()
     scanf("%d %d", &n1, &n2);
 
-    // store sum in variable Sum
-    Sum = n1 + n2;
+    // store Sum in variable SumOfTwoNums
+    SumOfTwoNums = n1 + n2;
 
     // display output     
-    printf("%d + %d = %d\n", n1, n2, Sum);
+    printf("%d + %d = %d\n", n1, n2, SumOfTwoNums);
 	
-	// check if the sum is even or odd, 
+	// check if the SumOfTwoNums is even or odd, 
     // and print result to screen
-    if(Sum % 2 == 0) printf("%d is even\n", Sum);
-    else printf("%d is odd\n", Sum);
+    if(SumOfTwoNums % 2 == 0) printf("%d is even\n", SumOfTwoNums);
+    else printf("%d is odd\n", SumOfTwoNums);
 
     return 0;
 }
